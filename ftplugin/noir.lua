0236c9d90d17b9b9ab8163400e2c3d0d0f8d3f23
filip <filip.harald@gmail.com>
2024-08-24 13:33:50 +0200

    changed to using Noir's package manager
    [1m[4mUsage:[0m [1mnargo[0m <COMMAND>
    
    [1m[4mCommands:[0m
      [1mcheck[0m    Checks the constraint system for errors [aliases: c]
      [1mfmt[0m      Format the Noir files in a workspace
      [1mcompile[0m  Compile the program and its secret execution trace into ACIR format
      [1mnew[0m      Create a Noir project in a new directory
      [1minit[0m     Create a Noir project in the current directory
      [1mexecute[0m  Executes a circuit to calculate its return value [aliases: e]
      [1mdebug[0m    Executes a circuit in debug mode
      [1mtest[0m     Run the tests for this program [aliases: t]
      [1minfo[0m     Provides detailed information on each of a program's function (represented by a single circuit) [aliases: i]
      [1mlsp[0m      Starts the Noir LSP server
      [1mhelp[0m     Print this message or the help of the given subcommand(s)
    
    [1m[4mOptions:[0m
      [1m-h[0m, [1m--help[0m     Print help
      [1m-V[0m, [1m--version[0m  Print version

--- ftplugin/noir.lua
+++ ftplugin/noir.lua
@@ -1,1 +1,1 @@
-vim.lsp.start({ cmd = {'nargo', 'lsp'}, root_dir = vim.fs.dirname(vim.fs.find({'Nargo.toml'}, {stop = vim.env.HOME})[1]), })
+vim.lsp.start({ cmd = {'aztec-nargo', 'lsp'}, root_dir = vim.fs.dirname(vim.fs.find({'Nargo.toml'}, {stop = vim.env.HOME})[1]), })
